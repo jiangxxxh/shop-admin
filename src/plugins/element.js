9aafcb9da71732e0adc796a6d56bc06c64ee9f0e
jiangxxxh <786628143@qq.com>
2021-09-08 16:43:25 +0800

    登录退出完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+// Message 弹框组件
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
